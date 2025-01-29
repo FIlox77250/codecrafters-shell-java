065f48bde190cad6ee02fc711ede99a3dbfaea62
FIlox77250 <filox77250@gmail.com>
2025-01-29 09:19:31 +0100

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        while (true) { 
-            System.out.print("$ ");  
-            String input = scanner.nextLine();
-            
+
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine().trim();
+
+
+            if (input.equals("exit 0")) {
+                scanner.close();
+                System.exit(0);
+            }
+
             System.out.println(input + ": command not found");
         }
     }
