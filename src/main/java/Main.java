4263d95164cf92e7a68a24b10af0b5db62b5cf0d
FIlox77250 <filox77250@gmail.com>
2025-01-30 09:40:05 +0100

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Set<String> builtins = Set.of("echo", "exit", "type");
 
         while (true) {
             System.out.print("$ ");
@@ -12,11 +15,21 @@ public class Main {
                 scanner.close();
                 System.exit(0);
             }
-
-                if (input.startsWith("echo ")) {
+            if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
                 continue;
             }
+
+            if (input.startsWith("type ")) {
+                String command = input.substring(5).trim();
+                if (builtins.contains(command)) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
+                continue;
+            }
+
             System.out.println(input + ": command not found");
         }
     }
